07e58d0a79f9b2f4edac83ca43b9bec3c2236cf1
raulcamunas <raulcamunas369@gmail.com>
2024-05-03 15:55:36 +0200

    proyecto reloj

---
+++ MODULO-3/project-reloj/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Countdown from './components/Countdown'
+import Cronometer from './components/Cronometer'
+import DigitalClock from './components/DigitalClock'
+
+
+function App() {
+
+  return (
+    <>
+      <Countdown />
+      <Cronometer />
+      <DigitalClock />
+    </>
+  )
+}
+
+export default App
